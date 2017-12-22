d8d197099b908d3ca7aa66a2ab2a9e62a497ac08
qryuu <qryuu.maoto@gmail.com>
2017-12-22 12:12:55 +0900

    Amazon Linux2のリポジトリパスを変更しました。

--- UserData/Launch-ZabbixServer-on-AmazonLinux.sh
+++ UserData/Launch-ZabbixServer-on-AmazonLinux.sh
@@ -2,17 +2,17 @@
 #変数宣言
 version=3.0 #Zabbixメジャーバージョン
 minorversion=latest #Zabbixマイナーバージョン or latest
-amazonlinux=amzn1 #AmaznLinuxバージョン amzn1 or amzn2
-proxyname= #ZabbixProxy名
-agentname= #ZabbixAgent名
-server= #ZabbixServerIPorFQDN
+amazonlinux=amzn2 #AmaznLinuxバージョン amzn1 or amzn2
+database=ec2 #利用BD ec2 or RDS or Aurora
+agentname=zabbix-server #ZabbixAgent名
+dbhost=localhost #DBホスト
 dbname=zabbix #ZabbixDB名
 dbuser=zabbix #ZabbixDBユーザ名
 dbpassword=zabbix #ZabbixDBパスワード
-encryption= #暗号化方式 unencrypted or psk or cert
+encryption=unencrypted #暗号化方式 unencrypted or psk or cert
 #暗号化方式pskの場合入力
 pskid= #pre-shared keysアイデンティティ
-#暗号化方式certの場合入力　ファイルをダウンロード可能な場所に配置してください。
+#暗号化方式certの場合入力 ※ファイルをダウンロード可能な場所に配置してください。
 cafile= #CA証明書URL
 certfle= #Server証明書URL
 keyfile= #秘密鍵ファイルURL
@@ -33,9 +33,9 @@ fi
 yum update
 yum install --enablerepo=epel iksemel iksemel-devel -y
 if [ ${minorversion} = "latest" ] ; then
-yum install zabbix-proxy-mysql zabbix-java-gateway zabbix-agent zabbix-get zabbix-sender mysql56 mysql56-server -y
+yum install zabbix-server-mysql zabbix-web-mysql zabbix-web-japanese zabbix-java-gateway zabbix-agent zabbix-get zabbix-sender mysql56 mysql56-server httpd24 -y
 else
-yum install zabbix-proxy-mysql-${minorversion} zabbix-java-gateway-${minorversion} zabbix-agent-${minorversion} zabbix-get-${minorversion} zabbix-sender-${minorversion} mysql56 mysql56-server -y
+yum install zabbix-server-mysql-${minorversion} zabbix-web-mysql-${minorversion} zabbix-web-japanese-${minorversion} zabbix-java-gateway-${minorversion} zabbix-agent-${minorversion} zabbix-get-${minorversion} zabbix-sender-${minorversion} mysql56 mysql56-server httpd24 -y
 fi
 #MySql起動
 service mysqld start
@@ -71,21 +71,17 @@ docversion=`docversioncheck`
 zcat "/usr/share/doc/zabbix-proxy-mysql-${docversion}/schema.sql.gz" | mysql --defaults-extra-file=/home/ec2-user/my.cnf-zabbix 
 echo "ALTER TABLE history ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8; ALTER TABLE history_log ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8; ALTER TABLE history_str ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8; ALTER TABLE history_text ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8; ALTER TABLE history_uint ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8; ALTER TABLE events ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8;" > /tmp/ALTERTABLE.sql
 mysql --defaults-extra-file=/home/ec2-user/my.cnf-zabbix < /tmp/ALTERTABLE.sql
-#ZabbixProxy設定
-sed -i -e "s/Server=127.0.0.1/Server=${server}/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/Hostname=Zabbix proxy/Hostname=${proxyname}/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/LogFileSize=0/LogFileSize=10/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/DBName=zabbix_proxy/DBName=${dbname}/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/DBUser=zabbix/DBUser=${dbuser}/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# DBPassword=/DBPassword=${dbpassword}/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# ProxyOfflineBuffer=1/ProxyOfflineBuffer=168/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# ConfigFrequency=3600/ConfigFrequency=300/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# StartPollers=5/StartPollers=10/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# StartPollersUnreachable=1/StartPollersUnreachable=3/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# StartPingers=1/StartPingers=5/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# StartDiscoverers=1/StartDiscoverers=3/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# StartHTTPPollers=1/StartHTTPPollers=3/g" /etc/zabbix/zabbix_proxy.conf
-sed -i -e "s/# JavaGateway=/# JavaGateway=127.0.0.1/g" /etc/zabbix/zabbix_proxy.conf
+#ZabbixServer設定
+sed -i -e "s/LogFileSize=0/LogFileSize=10/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/DBName=zabbix/DBName=${dbname}/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/DBUser=zabbix/DBUser=${dbuser}/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# DBPassword=/DBPassword=${dbpassword}/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# StartPollers=5/StartPollers=10/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# StartPollersUnreachable=1/StartPollersUnreachable=3/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# StartPingers=1/StartPingers=5/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# StartDiscoverers=1/StartDiscoverers=3/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# StartHTTPPollers=1/StartHTTPPollers=3/g" /etc/zabbix/zabbix_server.conf
+sed -i -e "s/# JavaGateway=/# JavaGateway=127.0.0.1/g" /etc/zabbix/zabbix_server.conf
 #ZabbixAgent設定
 sed -i -e "s/LogFileSize=0/LogFileSize=5/g" /etc/zabbix/zabbix_agentd.conf
 sed -i -e "s# EnableRemoteCommands=0/EnableRemoteCommands=1/g" /etc/zabbix/zabbix_agentd.conf
@@ -120,7 +116,8 @@ chown zabbix.zabbix /etc/zabbix/tls/*
 chmod 400 /etc/zabbix/tls/*
 fi
 #自動起動設定
-chkconfig zabbix-proxy on
+chkconfig zabbix-server on
+chkconfig httpd on
 chkconfig zabbix-agent on
 chkconfig zabbix-java-gateway on
 chkconfig mysqld on
